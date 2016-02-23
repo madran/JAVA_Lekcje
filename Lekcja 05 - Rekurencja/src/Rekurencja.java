f0ecd963634f07b115b8c9acbe85f0ae225eb80b
madran <lukasz.rozanski@gmail.com>
2016-02-23 16:06:21 +0100

    rekurencja - przeniesienie pliku

---
+++ Lekcja 05 - Rekurencja/src/Rekurencja.java
@@ -0,0 +1,32 @@
+public class Rekurencja {
+
+	public static void main(String[] args) {
+		rekurencja(10);
+
+		System.out.println(sumaIteracyjne(3));
+	}
+
+	static int rekurencja(int a) {
+		if (a > 0) {
+//			System.out.println(a);
+			return rekurencja(a - 1);
+		} else
+			return 0;
+	}
+
+	static int suma(int a) {
+		if(a < 1) return 0;
+
+		return a + suma(a-1);
+	}
+
+	static int sumaIteracyjne(int a) {
+		int suma = 0;
+		while(a > 0) {
+			suma = suma + a--;
+		}
+
+		return suma;
+	}
+
+}
